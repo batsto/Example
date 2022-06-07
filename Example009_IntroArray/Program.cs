13a86ef269592292d9376dd7d61c1103c8d681d6
batston <gbatston@gmail.com>
2022-06-07 20:18:16 +0300

    Example009 new

--- Example009_IntroArray/Program.cs
+++ Example009_IntroArray/Program.cs
@@ -1,17 +1,16 @@
-﻿int Max(int arg1, int arg2, int arg3)
-{
-    int result = arg1;
-    if (arg2 > result) result = arg2;
-    if (arg3 > result) result = arg3;
-    return result;
-}
- //              0    1   2   3   4   5   6   7   8
-int[] array = { 11, 211, 31, 41, 15, 61, 17, 18, 19 };
+﻿int[] array  = {1,24,35,48,75,68,74,83,94};
 
-int result = Max(
-    Max(array[0], array[1], array[2]),
-    Max(array[3], array[4], array[5]),
-    Max(array[6], array[7], array[8])
-);
-Console.WriteLine(result);
+int result(int x, int y, int z) 
+    {
+        int max = x;
+        if(y > x) max = y;
+        if(z > y) max = z;
+        return max;
+    }
 
+
+int max =result(result(array[0], array[1], array[2]),
+                result(array[3], array[4], array[5]),
+                result(array[6], array[7], array[8]));
+
+Console.WriteLine(max);
